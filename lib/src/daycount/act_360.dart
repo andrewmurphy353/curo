9c9d0c97818e50229f60b0d20041de4b629ac5bf
andrewmurphy353 <smurfsturf@pm.me>
2024-10-28 19:11:36 +0000

    Added new convention

---
+++ lib/src/daycount/act_360.dart
@@ -0,0 +1,51 @@
+import '../utilities/dates.dart';
+import 'convention.dart';
+import 'day_count_factor.dart';
+
+/// The Actual/360 day count convention which specifies that the number of
+/// days in the Calculation Period or Compounding Period in respect of which
+/// payment is being made is divided by 360.
+/// 
+class Act360 extends Convention {
+  /// Provides an instance of the Act/360 day count convention object.
+  ///
+  /// The default day count instance is suitable for use in all compound
+  /// interest calculations. With the default setup interest is calculated
+  /// on the reducing capital balance and is compounded at a frequency
+  /// typically determined by the time interval between cash flows.
+  ///
+  /// For non-compound interest calculations, such as solving for unknowns
+  /// on the basis of an eXtended Internal Rate of Return (XIRR), set
+  /// [useXirrMethod] to *true*. With this setup the day count is calculated
+  /// with reference to the first cash flow date in the series.
+  ///
+  /// [usePostDates] (optional) defines whether the day count between
+  /// cash flows is computed using cash flow post dates (true), or
+  /// alternatively cash flow value dates (false). Default is true.
+  ///
+  /// [includeNonFinancingFlows] (optional) determines whether non-financing
+  /// cash flows, such as charges or fees within the cash flow profile, are
+  /// included in the computation of periodic factors. Default is false.
+  ///
+  /// [useXirrMethod] (optional) determines whether to use the XIRR method
+  /// of determining time periods between cash flow dates (true). Default
+  /// is false.
+  const Act360({
+    bool usePostDates = true,
+    bool includeNonFinancingFlows = false,
+    bool useXirrMethod = false,
+  }) : super(
+            usePostDates: usePostDates,
+            includeNonFinancingFlows: includeNonFinancingFlows,
+            useXirrMethod: useXirrMethod);
+
+  @override
+  DayCountFactor computeFactor(DateTime d1, DateTime d2) {
+    final int numerator = actualDays(d1, d2);
+    final double factor = numerator / 360;
+    return DayCountFactor(
+      factor,
+      [DayCountFactor.operandsToString(numerator, 360)],
+    );
+  }
+}
---
+++ test/daycount/act_360_test.dart
@@ -0,0 +1,67 @@
+import 'package:curo/src/daycount/act_360.dart';
+import 'package:curo/src/daycount/day_count_origin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Act360.computeFactor', () {
+    const dc = Act360();
+    test('28/01/2020 to 28/02/2020', () {
+      final dcf = dc.computeFactor(
+        DateTime.utc(2020, 1, 28),
+        DateTime.utc(2020, 2, 28),
+      );
+      expect(dcf.factor, 0.08611111111111111);
+      expect(dcf.toString(), '(31/360) = 0.08611111');
+    });
+    test('28/01/2019 to 28/02/2019', () {
+      final dcf = dc.computeFactor(
+        DateTime.utc(2019, 1, 28),
+        DateTime.utc(2019, 2, 28),
+      );
+      expect(dcf.factor, 0.08611111111111111);
+      expect(dcf.toString(), '(31/360) = 0.08611111');
+    });
+    test('31/12/2017 to 31/12/2019', () {
+      final dcf = dc.computeFactor(
+        DateTime.utc(2017, 12, 31),
+        DateTime.utc(2019, 12, 31),
+      );
+      expect(dcf.factor, 2.0277777777777777);
+      expect(dcf.toString(), '(730/360) = 2.02777778');
+    });
+    test('31/12/2018 to 31/12/2020', () {
+      final dcf = dc.computeFactor(
+        DateTime.utc(2018, 12, 31),
+        DateTime.utc(2020, 12, 31),
+      );
+      expect(dcf.factor, 2.0305555555555554);
+      expect(dcf.toString(), '(731/360) = 2.03055556');
+    });
+    test('30/06/2019 to 30/06/2021', () {
+      final dcf = dc.computeFactor(
+        DateTime.utc(2019, 6, 30),
+        DateTime.utc(2021, 6, 30),
+      );
+      expect(dcf.factor, 2.0305555555555554);
+      expect(dcf.toString(), '(731/360) = 2.03055556');
+    });
+  });
+  group('Act360 default instance', () {
+    const dc = Act360();
+    test('dayCountMethod() to return "neighbour"', () {
+      expect(dc.dayCountOrigin(), DayCountOrigin.neighbour);
+    });
+    test('usePostDates to return "true"', () {
+      expect(dc.usePostDates, true);
+    });
+    test('includeNonFinancingFlows() to return "false"', () {
+      expect(dc.includeNonFinancingFlows, false);
+    });
+  });
+  group('Act360 useXirrMethod', () {
+    const dc = Act360(useXirrMethod: true);
+    test('dayCountMethod() to return "drawdown"', () {
+      expect(dc.dayCountOrigin(), DayCountOrigin.drawdown);
+    });
+  });
+}
